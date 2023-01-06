1123f5aff16943180a92ece331042a61ac485596
@Mike-Prybytkin <prybytkin@bk.ru>
2023-01-07 00:17:25 +0300

    feat: implement navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faBars, faClose } from '@fortawesome/free-solid-svg-icons';
+import Navigation from './Navigation';
 
 const Header = () => {
   const [openBurger, setOpenBurger] = useState(true);
-  console.log(openBurger);
   return (
     <header className="header">
       <a
@@ -29,7 +29,11 @@ const Header = () => {
           </span>
         </button>
       </a>
-      <div className="wrapper header-wrapper">
+      <div
+        className={`wrapper header-wrapper ${
+          openBurger ? '' : 'burger-active'
+        }`}
+      >
         <div className="header-wrapper__links">
           {openBurger ? (
             <button onClick={() => setOpenBurger(false)}>
@@ -83,7 +87,11 @@ const Header = () => {
               alt="icon-instagram"
             />
           </a>
-          <p className="article-date">Panamá, 06 de enero del 2023</p>
+          {openBurger ? (
+            <p className="article-date">Panamá, 06 de enero del 2023</p>
+          ) : (
+            ''
+          )}
         </div>
         <div className="header-wrapper__logo">
           <a href="https://www.prensa.com/" target="_blank" rel="noreferrer">
@@ -96,16 +104,240 @@ const Header = () => {
         <div className="header-wrapper__buttons">
           <button className="button button_bordered">Inicia Sesión</button>
           <button className="button button_colored">Suscríbete desde $1</button>
-          <a
-            className="link"
-            href="https://multimedia.corprensa.com/multimedia/ediciones/la_prensa/2023-01-06/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            ePaper
-          </a>
+          {openBurger ? (
+            <a
+              className="link"
+              href="https://multimedia.corprensa.com/multimedia/ediciones/la_prensa/2023-01-06/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              ePaper
+            </a>
+          ) : (
+            ''
+          )}
         </div>
+        {!openBurger ? (
+          <div className="link-items layout-3-column">
+            <div className="link-items__column">
+              <h4 className="link-items__heading">SECCIONES</h4>
+              <div>
+                <a className="link" href="https://www.prensa.com/">
+                  Portada
+                </a>
+                <a className="link" href="https://www.prensa.com/locales/">
+                  Locales
+                </a>
+                <a className="link" href="https://www.prensa.com/sociedad/">
+                  Sociedad
+                </a>
+                <a className="link" href="https://www.prensa.com/provincias/">
+                  Provincias
+                </a>
+                <a className="link" href="https://www.prensa.com/judiciales/">
+                  Judiciales
+                </a>
+                <a className="link" href="https://www.prensa.com/politica/">
+                  Política
+                </a>
+                <a className="link" href="https://www.prensa.com/economia/">
+                  Economía
+                </a>
+                <a className="link" href="https://www.prensa.com/mundo/">
+                  Mundo
+                </a>
+                <a className="link" href="https://www.prensa.com/deportes/">
+                  Deportes
+                </a>
+                <a className="link" href="https://www.prensa.com/vivir/">
+                  Vivir+
+                </a>
+                <a className="link" href="https://www.prensa.com/status-k/">
+                  Status K
+                </a>
+                <a className="link" href="https://www.prensa.com/opinion/">
+                  Opinión
+                </a>
+                <a className="link" href="https://www.prensa.com/blogs/">
+                  Blogs
+                </a>
+                <a className="link" href="https://www.prensa.com/podcasts/">
+                  Podcasts
+                </a>
+                <a className="link" href="https://www.prensa.com/fotogalerias/">
+                  Fotografía
+                </a>
+                <a className="link" href="https://www.prensa.com/videos/">
+                  Videos
+                </a>
+                <a className="link" href="https://www.prensa.com/infografias/">
+                  Infografias
+                </a>
+                <a className="link" href="https://www.prensa.com/correcciones/">
+                  Correcciones
+                </a>
+              </div>
+            </div>
+            <div className="link-items__column">
+              <h4 className="link-items__heading">SERVICIOS</h4>
+              <div>
+                <a
+                  className="link"
+                  href="https://multimedia.corprensa.com/multimedia/ediciones/la_prensa/2023-01-06/"
+                >
+                  ePaper
+                </a>
+                <a className="link" href="https://www.prensa.com/buscador/">
+                  Buscador
+                </a>
+                <a
+                  className="link"
+                  href="https://www.prensa.com/autor/Corprensa%20Content%20Studio/"
+                >
+                  Corprensa Content Studio
+                </a>
+                <a
+                  className="link"
+                  href="https://www.prensa.com/contenido-patrocinado/"
+                >
+                  Contenido patrocinado
+                </a>
+                <a
+                  className="link"
+                  href="https://www.prensa.com/resena-digital/"
+                >
+                  Reseña Digital
+                </a>
+                <a className="link" href="https://www.prensa.com/comunicados/">
+                  Comunicados
+                </a>
+                <a className="link" href="https://www.prensa.com/catalogos/">
+                  Catálogos y Otras Publicaciones
+                </a>
+                <a className="link" href="https://www.prensa.com/loterias/">
+                  Lotería
+                </a>
+                <a className="link" href="https://www.prensa.com/horoscopo/">
+                  Horóscopo
+                </a>
+                <a
+                  className="link"
+                  href="https://suscribete.prensa.com/kiosco/?_gl=1*1g03922*_ga*Mzk4NTI3MDQwLjE2NzI5MzE4MjM.*_ga_D6E1T76QJ4*MTY3MzAwNjExMC44LjAuMTY3MzAwNjEyMi40OC4wLjA."
+                >
+                  Kiosco Digital
+                </a>
+                <a className="link" href="https://www.prensa.com/luctuosas/">
+                  Luctuosas
+                </a>
+                <a className="link" href="https://www.prensa.com/cine/">
+                  Cine
+                </a>
+                <a className="link" href="https://www.prensa.com/arcio/rss/">
+                  RSS
+                </a>
+                <a className="link" href="https://www.prensa.com/temas/">
+                  Temas
+                </a>
+                <a className="link" href="https://www.prensa.com/autores/">
+                  Autores
+                </a>
+                <a
+                  className="link"
+                  href="https://www.prensa.com/notas-al-lector/"
+                >
+                  Notas al lector
+                </a>
+                <a
+                  className="link"
+                  href="https://www.prensa.com/buenas-practicas/"
+                >
+                  Buenas prácticas
+                </a>
+              </div>
+            </div>
+            <div className="link-items__column">
+              <h4 className="link-items__heading">OTROS SITIOS</h4>
+              <div>
+                <a
+                  className="link"
+                  href="https://www.asistentefinanciero.com/?utm_source=prensacom&utm_medium=burger-menu&utm_campaign=referral"
+                >
+                  Asistente Financiero
+                </a>
+                <a
+                  className="link"
+                  href="https://www.metropormetro.com/?utm_source=prensacom&utm_medium=burger-menu&utm_campaign=referral"
+                >
+                  Metro por Metro
+                </a>
+                <a
+                  className="link"
+                  href="https://www.tiendapanama.com/?utm_source=prensacom&utm_medium=burger-menu&utm_campaign=referral"
+                >
+                  Tienda Panamá
+                </a>
+                <a
+                  className="link"
+                  href="https://www.tustiquetes.com/?utm_source=prensacom&utm_medium=burger-menu&utm_campaign=referral"
+                >
+                  Tus Tiquetes
+                </a>
+                <a
+                  className="link"
+                  href="https://www.cerokm.com/?utm_source=prensacom&utm_medium=burger-menu&utm_campaign=referral"
+                >
+                  Cero KM
+                </a>
+                <a
+                  className="link"
+                  href="http://corporativo.prensa.com/?utm_source=prensacom&utm_medium=burger-menu&utm_campaign=referral"
+                >
+                  Corprensa
+                </a>
+                <a
+                  className="link"
+                  href="https://www.midiario.com/?utm_source=prensacom&utm_medium=burger-menu&utm_campaign=referral"
+                >
+                  Mi Diario
+                </a>
+                <a
+                  className="link"
+                  href="https://www.martesfinanciero.com/?utm_source=prensacom&utm_medium=burger-menu&utm_campaign=referral"
+                >
+                  Martes Financiero
+                </a>
+                <a
+                  className="link"
+                  href="https://www.ellas.pa/?utm_source=prensacom&utm_medium=burger-menu&utm_campaign=referral"
+                >
+                  Ellas
+                </a>
+                <a
+                  className="link"
+                  href="https://club.prensa.com/?utm_source=prensacom&utm_medium=burger-menu&utm_campaign=referral"
+                >
+                  Club La Prensa
+                </a>
+                <a
+                  className="link"
+                  href="https://buscafacil.com/?utm_source=prensacom&utm_medium=burger-menu&utm_campaign=referral"
+                >
+                  Busca Fácil
+                </a>
+                <a
+                  className="link"
+                  href="https://www.alamesa.pa/?utm_source=prensacom&utm_medium=burger-menu&utm_campaign=referral"
+                >
+                  A la Mesa
+                </a>
+              </div>
+            </div>
+          </div>
+        ) : (
+          ''
+        )}
       </div>
+      <Navigation />
     </header>
   );
 };
---
+++ src/components/Navigation.js
@@ -0,0 +1,201 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
+
+const Navigation = () => {
+  const [openDropDownList, setOpenDropDownList] = useState(false);
+  const [dropDownName, setDropDownName] = useState('Locales');
+
+  const dropdownListToggle = (e) => {
+    setDropDownName('');
+    const targetDropDownName = e.target.textContent;
+    if (targetDropDownName === 'Locales') {
+      setDropDownName(targetDropDownName);
+    } else if (targetDropDownName === 'Economía') {
+      setDropDownName(targetDropDownName);
+    } else if (targetDropDownName === 'Multimedia') {
+      setDropDownName(targetDropDownName);
+    }
+    setOpenDropDownList(!openDropDownList);
+  };
+
+  const popularNewsListener = (e) => {
+    if (e.relatedTarget.classList.contains('dropdown-list')) {
+      setOpenDropDownList(true);
+    }
+  } 
+
+  const close = () => {
+    setOpenDropDownList(false);
+  }
+  return (
+    <div className="wrapper nav-wrapper">
+      <nav className={`nav ${openDropDownList ? 'dropdown-active' : ''}`}>
+        <ul className="nav__items">
+          <li
+            className="nav__item dropdown-list"
+            onMouseEnter={dropdownListToggle}
+            onMouseLeave={dropdownListToggle}
+          >
+            <span>Locales</span>
+          </li>
+          <FontAwesomeIcon className="fontawersome-ico" icon={faCaretDown} />
+          <li className="nav__item">
+            <a className="link" href="https://www.prensa.com/judiciales/">
+              Judiciales
+            </a>
+          </li>
+          <li className="nav__item">
+            <a className="link" href="https://www.prensa.com/politica/">
+              Politica
+            </a>
+          </li>
+          <li
+            className="nav__item dropdown-list"
+            onMouseEnter={dropdownListToggle}
+            onMouseLeave={dropdownListToggle}
+          >
+            <span>Economía</span>
+          </li>
+          <FontAwesomeIcon className="fontawersome-ico" icon={faCaretDown} />
+          <li className="nav__item">
+            <a className="link" href="https://www.prensa.com/mundo/">
+              Mundo
+            </a>
+          </li>
+          <li className="nav__item">
+            <a className="link" href="https://www.prensa.com/deportes/">
+              Deportes
+            </a>
+          </li>
+          <li className="nav__item">
+            <a className="link" href="https://www.prensa.com/vivir/">
+              Vivir+
+            </a>
+          </li>
+          <li className="nav__item">
+            <a className="link" href="https://www.prensa.com/status-k/">
+              Status K
+            </a>
+          </li>
+          <li className="nav__item">
+            <a className="link" href="https://www.prensa.com/podcasts/">
+              Podcasts
+            </a>
+          </li>
+          <li className="nav__item">
+            <a className="link" href="https://www.prensa.com/opinion/">
+              Opinión
+            </a>
+          </li>
+          <li
+            className="nav__item dropdown-list"
+            onMouseEnter={dropdownListToggle}
+            onMouseLeave={dropdownListToggle}
+          >
+            <span>Multimedia</span>
+          </li>
+          <FontAwesomeIcon className="fontawersome-ico" icon={faCaretDown} />
+          <li className="nav__item">
+            <a className="link" href="https://www.prensa.com/correcciones/">
+              Correcciones
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div className="popular-news" onMouseOver={popularNewsListener} onMouseLeave={close}>
+        {!openDropDownList ? (
+          <div className="popular-news__de-hoy">
+            <span className="popular-news__heading">TEMAS DE HOY:</span>
+            <a
+              className="link popular-news__link"
+              href="https://www.prensa.com/tema/tribunal-electoral-te/"
+            >
+              Tribunal Electoral ...
+            </a>
+            <a
+              className="link popular-news__link"
+              href="https://www.prensa.com/tema/proceso-civil/"
+            >
+              Proceso Civil
+            </a>
+            <a
+              className="link popular-news__link"
+              href="https://www.prensa.com/tema/kenia-porcell/"
+            >
+              Kenia Porcell
+            </a>
+            <a
+              className="link popular-news__link"
+              href="https://www.prensa.com/tema/juan-carlos-varela/"
+            >
+              Juan Carlos Varela
+            </a>
+            <a
+              className="link popular-news__link"
+              href="https://www.prensa.com/tema/enfermedades/"
+            >
+              Enfermedades
+            </a>
+            <a
+              className="link popular-news__link"
+              href="https://www.prensa.com/tema/turismo/"
+            >
+              Turismo
+            </a>
+            <a
+              className="link popular-news__link"
+              href="https://www.prensa.com/tema/viajes/"
+            >
+              Viajes
+            </a>
+            <a
+              className="link popular-news__link"
+              href="https://www.prensa.com/tema/caja-de-seguro-social/"
+            >
+              Caja De Seguro Socia...
+            </a>
+            <a
+              className="link popular-news__link"
+              href="https://www.prensa.com/tema/jubilados/"
+            >
+              Jubilados
+            </a>
+          </div>
+        ) : (
+          ''
+        )}
+        {openDropDownList && dropDownName === 'Locales' ? (
+          <div className="popular-news__locales">
+            <a className='link' href="https://www.prensa.com/sociedad/">Sociedad</a>
+            <a className='link' href="https://www.prensa.com/provincias/">Provincias</a>
+            <a className='link' href="https://www.prensa.com/comunicados/">Comunicados</a>
+            <a className='link' href="https://www.prensa.com/luctuosas/">Luctuosas</a>
+          </div>
+        ) : (
+          ''
+        )}
+        {openDropDownList && dropDownName === 'Economía' ? (
+          <div className="popular-news__economia">
+            <a className='link' href="https://www.prensa.com/notas-de-prensa/">Notas de Prensa Corporativas</a>
+            <a className='link' href="https://www.prensa.com/martes-financiero/">Martes Financiero</a>
+          </div>
+        ) : (
+          ''
+        )}
+        {openDropDownList && dropDownName === 'Multimedia' ? (
+          <div className="popular-news__multimedia">
+            <a className='link' href="https://www.prensa.com/podcasts/">Podcasts</a>
+            <a className='link' href="https://www.prensa.com/videos/">Videos</a>
+            <a className='link' href="https://www.prensa.com/fotogalerias/">Fotografía</a>
+            <a className='link' href="https://www.prensa.com/correcciones/">Infografías</a>
+          </div>
+        ) : (
+          ''
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
